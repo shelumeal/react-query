12fb0d2a17060f9c8cac3adb574b79deb521e911
Shelumiel <sssharon7@gmail.com>
2022-09-04 15:21:04 +0800

    Add usequery

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { Route, Routes } from "react-router";
+import { QueryClientProvider, QueryClient } from "react-query";
 import "./App.css";
 import HomePage from "./components/Home.page";
 import Navbar from "./components/Navbar";
 import RQSuperheroesPage from "./components/RQSuperheroes.page";
 import SuperheroesPage from "./components/Superheroes.page";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="super-heroes" element={<SuperheroesPage />}></Route>
         <Route path="rq-super-heroes" element={<RQSuperheroesPage />}></Route>
       </Routes>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperheroes.page.js
+++ src/components/RQSuperheroes.page.js
@@ -1,5 +1,25 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchHeroes = () => {
+  return axios.get("http://localhost:4000/superheros");
+};
+
 function RQSuperheroesPage() {
-  return <div>RQ Super Heroes Page</div>;
+  const { isLoading, data } = useQuery("super-heroes", fetchHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 }
 
 export default RQSuperheroesPage;
