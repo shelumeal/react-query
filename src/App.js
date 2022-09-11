c4d0b2a115dea904dd96054070840b24123542cb
Shelumiel <sssharon7@gmail.com>
2022-09-11 09:26:30 +0800

    Add Infinite Queries

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ParallelQueriesPage from "./components/ParallelQueries.page";
 import { DynamicParallelPage } from "./components/DynamicParallel.page";
 import { DependentQueriesPage } from "./components/DependentQueries.page";
 import { PaginatedQueriesPage } from "./components/PaginatedQueries.page";
+import { InfiniteQueriesPage } from "./components/InfiniteQueries.page";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/rq-infinite" element={<InfiniteQueriesPage />}></Route>
         <Route path="/rq-paginated" element={<PaginatedQueriesPage />}></Route>
         <Route
           path="/rq-dependent"
